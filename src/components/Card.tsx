5c65c7c4d9e81c7020a8089fc1bfaff753520c36
murphybread <nargene@gmail.com>
2024-11-19 14:40:11 +0900

    feat(card): use image front and back

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import '@styles/card.css';
 
-// 애니메이션 상태에 따라 표시할 텍스트를 객체로 관리
-const frontText: Record<number, string>  = {
-  0: 'Start Front',
-  1: '?',
-  2: '?',
-  3: 'End Front',
+const frontImages: Record<number, string> = {
+  0: '/images/initial_front.jpg', // 앞면 이미지
+  1: '/images/question_1.jpg',
+  2: '/images/question_1.jpg',
+  3: 'https://storage.googleapis.com/magic-prompt-well/images/A%20small%2C%20twinkling%20star%20with%20a%20gentle%20golden%20glow.%20It%20floats%20just%20above%20the%20caster%27s%20shoulder%2C%20leaving%20a%20trail%20of%20shimmering%20stardust.%20Its%20light%20is%20soothing%20and%20warm%2C%20capable%20of%20providing%20guidance%20in%20the%20darkest%20of%20nights.png',
 };
 
-
+const backImages: string = '/images/card_back.jpg'; // 뒷면 이미지
 
 const Card: React.FC = () => {
-  const [animationStep, setAnimationStep] = useState(0); // 애니메이션 상태 추적
+  const [animationStep, setAnimationStep] = useState(0); // 클릭 상태 추적
+  const isBackVisible = animationStep >= 1 && animationStep <= 2; // 2~3단계에서는 뒷면 보임
 
   const flipCard = () => {
-    setAnimationStep((prevStep) => (prevStep + 1) % 4); // 4단계 순차적으로 변경
+    setAnimationStep((prevStep) => (prevStep + 1) % 4); // 단계 순환: 0 → 1 → 2 → 3 → 0
   };
 
   return (
@@ -26,14 +26,22 @@ const Card: React.FC = () => {
         ${animationStep === 3 ? 'growing' : ''}`}
       onClick={flipCard}
     >
-      <div className="card-border">
-        <div className="card-inner">
+      <div className="card-border"
+      style = {{
+          transform:
+            animationStep === 0 || animationStep === 3
+              ? 'rotateY(0deg)' // 초기 또는 멈춘 상태: 앞면만 보임
+              : 'rotateY(360deg)', // 회전 중: 앞뒤면 번갈아 보임
+      }} >
+        <div className="card-inner" style={{ transform: isBackVisible ? 'rotateY(180deg)' : 'rotateY(0deg)' }}>
           <div className="card-front">
-            <h2>{frontText[animationStep]}</h2>
+            <img src={frontImages[animationStep]} alt={`Card front ${animationStep}`} className="card-image" />
           </div>
-          <div className="card-back">
-            <h2>Back</h2>
-          </div>
+          {isBackVisible && ( // 뒷면 표시 여부 조건
+            <div className="card-back">
+              <img src={backImages} alt="Card back" className="card-image" />
+            </div>
+          )}
         </div>
       </div>
     </div>
