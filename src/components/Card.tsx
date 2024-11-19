33c2b37a63643c7e136df9d5ae85527dd58c2063
murphybread <nargene@gmail.com>
2024-11-19 16:17:49 +0900

    fix(card): spin properly front and back

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,6 @@ const backImages: string = '/images/card_back.jpg'; // 뒷면 이미지
 
 const Card: React.FC = () => {
   const [animationStep, setAnimationStep] = useState(0); // 클릭 상태 추적
-  const isBackVisible = animationStep >= 1 && animationStep <= 2; // 2~3단계에서는 뒷면 보임
 
   const flipCard = () => {
     setAnimationStep((prevStep) => (prevStep + 1) % 4); // 단계 순환: 0 → 1 → 2 → 3 → 0
@@ -26,22 +25,14 @@ const Card: React.FC = () => {
         ${animationStep === 3 ? 'growing' : ''}`}
       onClick={flipCard}
     >
-      <div className="card-border"
-      style = {{
-          transform:
-            animationStep === 0 || animationStep === 3
-              ? 'rotateY(0deg)' // 초기 또는 멈춘 상태: 앞면만 보임
-              : 'rotateY(360deg)', // 회전 중: 앞뒤면 번갈아 보임
-      }} >
-        <div className="card-inner" style={{ transform: isBackVisible ? 'rotateY(180deg)' : 'rotateY(0deg)' }}>
+      <div className="card-border">
+        <div className="card-inner">
           <div className="card-front">
             <img src={frontImages[animationStep]} alt={`Card front ${animationStep}`} className="card-image" />
           </div>
-          {isBackVisible && ( // 뒷면 표시 여부 조건
             <div className="card-back">
               <img src={backImages} alt="Card back" className="card-image" />
             </div>
-          )}
         </div>
       </div>
     </div>
